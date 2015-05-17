582efc341eee0fe165b9a2871e7a6d5220e0fae6
Quinton McCombs <quinton.mccombs@gmail.com>
2015-05-17 15:41:10 -0500

    p6spy-290: lazy init for FileLogger

--- src/main/java/com/p6spy/engine/spy/appender/FileLogger.java
+++ src/main/java/com/p6spy/engine/spy/appender/FileLogger.java
@@ -18,20 +18,43 @@
  * #L%
  */
 package com.p6spy.engine.spy.appender;
+
+import com.p6spy.engine.spy.P6SpyOptions;
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 
-import com.p6spy.engine.spy.P6SpyOptions;
-
 public class FileLogger extends StdoutLogger {
 
-    public void setLogfile(String fileName) {
-    	try {
-    	    qlog = new PrintStream(new FileOutputStream(fileName, P6SpyOptions.getActiveInstance().getAppend()));
-    	} catch (IOException e) {
-    	    e.printStackTrace(System.err);
-    	}
+  private String fileName = null;
+  private PrintStream printStream = null;
+
+  protected void init() {
+    synchronized (FileLogger.class) {
+      if (printStream == null) {
+        if( fileName == null ) {
+          throw new IllegalStateException("setLogfile() must be called before init()");
+        }
+        try {
+          printStream = new PrintStream(new FileOutputStream(fileName, P6SpyOptions.getActiveInstance().getAppend()));
+        } catch (IOException e) {
+          e.printStackTrace(System.err);
+        }
+      }
     }
+  }
+
+  @Override
+  protected PrintStream getStream() {
+    if( printStream == null ) {
+      init();
+    }
+    return getStream();
+  }
+
+  public void setLogfile(String fileName) {
+    this.fileName = fileName;
+  }
 }
 
